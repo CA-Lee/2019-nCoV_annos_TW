80c4b652071f909ab6a5bce04ce88652d373d991
Samuel C.A. Lee <iamsamuellee92@gmail.com>
2020-02-08 17:37:09 +0800

    Add feature: archive and log system

--- make.sh
+++ make.sh
@@ -3,5 +3,9 @@ echo "start"
 [ -f "picpack.zip" ] && rm picpack.zip && echo "rm picpack.zip"
 cd ./annos
 zip ../picpack ./* > /dev/null
+cd ..
+cp picpack.zip ./archived/`date +%m%d%H%M%S`.zip
+echo "`date +%F_%H:%M:%S`: make.sh : Made new picpack.zip." >> log.html
+echo "`date +%F_%H:%M:%S`: make.sh : Contents of picpack.zip : `ls annos/ -1 | tr -s '\n' ' '`" >> log.html
 echo "done"
 exit 0
