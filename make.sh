7791229e8619873dc16462e96b77b0cad3857a25
Samuel C.A. Lee <iamsamuellee92@gmail.com>
2020-02-10 02:20:23 +0800

    fix bug about log

--- make.sh
+++ make.sh
@@ -6,8 +6,8 @@ zip ../picpack ./* > /dev/null
 cd ..
 cp picpack.zip ./archived/`date +%m%d%H%M%S`.zip
 
-echo "`date +%F_%H:%M:%S`: make.sh : Made new picpack.zip." >> log.html
-echo "`date +%F_%H:%M:%S`: make.sh : Contents of picpack.zip : `ls annos/ -1 | tr -s '\n' ' '`" >> log.html
+echo "`date +%F_%H:%M:%S`: make.sh : Made new picpack.zip.<br>" >> log.html
+echo "`date +%F_%H:%M:%S`: make.sh : Contents of picpack.zip : `ls annos/ -1 | tr -s '\n' ' '`<br>" >> log.html
 sed -Ei "s/Last update: [0-9]{10}/Last update: `date +%m%d%H%M%S`/g" index.html
 
 git add .
